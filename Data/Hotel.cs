b5394883a8bf09fa9d070f24380f97ef1077c237
WeyehnR <weyehn1@gmail.com>
2026-05-15 09:02:38 -0400

    adds notes to gitignores

--- Data/Hotel.cs
+++ Data/Hotel.cs
@@ -1,16 +1,5 @@
 ﻿namespace HotelListing_API.Data;
 
-
-//This is a Data Model class that represents a Hotel entity.
-//It has properties for Id, Name, Address, and Rating.
-
-//What is a Data model class?
-//A Data model class is a class that represents the structure of data in an application.
-//It defines the properties and relationships of the data entities that are used in the
-//application. In the context of an API, a Data model class is often used to represent
-//the data that is sent and received through the API endpoints.
-//It serves as a blueprint for how the data should be structured and can be used for
-//validation, serialization, and deserialization of data when communicating with clients.
 public class Hotel
 {
     public int Id { get; set; }
@@ -26,29 +15,3 @@ public class Hotel
     public Country? Country { get; set; }
 
 }
-
-//An auto-implemented property is a shorthand syntax for defining a property in C#.
-//It allows you to quickly create a property without having to explicitly define a backing field.
-//The compiler automatically generates a private, anonymous backing field for the property, which is used
-//to store the value of the property.
-
-//the syntax for an auto-implemented property is as follows:
-//<access_modifier> <data_type> <property_name> { get; set; }
-
-//now within that syntax, you can also include an initializer to set a default value for the property. For example:
-//public string Name { get; set; } = "Default Name";
-
-//Now, when you create an instance of the class that contains this property, the Name property will be initialized with
-//the value "Default Name" unless you explicitly set it to something else.
-
-//the 'get' accessor is used to retrieve the value of the property, while the 'set' accessor is used to assign a value to the property.
-
-//Here is how you can use this Hotel class:
-//Hotel hotel = new Hotel();
-//hotel.Id = 1;
-//hotel.Name = "Grand Hotel";
-//hotel.Address = "123 Main Street";
-//hotel.Rating = 4.5;
-
-
-//in the end, they are syntactic sugar that allows you to write cleaner and more concise code when defining properties in C#.
