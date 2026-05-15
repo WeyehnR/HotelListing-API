9fc2f34b0c2e326d2d699f7bc0fa97a786f43521
WeyehnR <weyehn1@gmail.com>
2026-05-15 11:29:18 -0400

    set up a connection string

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using HotelListing_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options => options.UseSqlServer(connectionString));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
